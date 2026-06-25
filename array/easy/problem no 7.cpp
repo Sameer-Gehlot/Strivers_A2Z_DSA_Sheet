f2b5feff4861b4169291852aa572cfbc6b281d4d
sameer <sameerkumar092005@gmail.com>
2026-06-25 23:07:48 +0530

    commit 3rd

---
+++ array/easy/problem no 7.cpp
@@ -0,0 +1,18 @@
+// Move Zeros to End
+    class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+       int n = nums.size();
+       int j=0;
+       for(int i=0;i<n;i++)
+       {
+           if(nums[i]!=0)
+           {
+               swap(nums[i],nums[j]);
+               j++;
+           }
+       }
+    }
+};
